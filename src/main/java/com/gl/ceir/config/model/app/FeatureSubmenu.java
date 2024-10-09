58032eee0408e3a8a5a0d84862b4ec8fd1f6ce3c
DMC <surendra.singh20.in@gmail.com>
2024-10-09 16:05:20 +0700

    Update FeatureSubmenu.java

--- src/main/java/com/gl/ceir/config/model/app/FeatureSubmenu.java
+++ src/main/java/com/gl/ceir/config/model/app/FeatureSubmenu.java
@@ -20,30 +20,8 @@ public class FeatureSubmenu {
 
     private int status;
 
-    //    @JsonBackReference
- //  @ManyToOne( fetch = FetchType.LAZY)
-//    @JoinColumn(name = "feature_menu_id")
-//    private FeatureMenu featureMenuId;
+     
     @JsonIgnore
     private Integer featureMenuId;
 }
-
-//  @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, optional = false)
-//   @JoinColumn(name = "feature_list_id", nullable = false)
-//  @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = false)
-//    @JsonIgnore
-//    @ManyToOne
-//    @JoinColumn(name = "featureMenuId")
-//    private FeatureMenu featureMenu;
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (!(o instanceof FeatureSubmenu)) return false;
-//        FeatureSubmenu that = (FeatureSubmenu) o;
-//        return status == that.status && Objects.equals(id, that.id) && Objects.equals(featureId, that.featureId) && Objects.equals(link, that.link) && Objects.equals(name, that.name);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(id, featureId, link, name, status);
-//    }
+ 
