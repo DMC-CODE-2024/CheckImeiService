ef35599dc6ad60f80e0d06930cf2d6d4d827ed0d
DMC-CODE-2024 <mojbhar20@gmail.com>
2025-01-22 09:07:56 +0530

    Bug fix

--- src/main/java/com/gl/ceir/config/model/constants/Alerts.java
+++ src/main/java/com/gl/ceir/config/model/constants/Alerts.java
@@ -1,13 +1,13 @@
 package com.gl.ceir.config.model.constants;
 
 public enum Alerts {
-    ALERT_1103("alert1103"), // check imei fail
-    ALERT_1104("alert1104"), // saveCheckImeiRequest ,appDeviceDetailsRepo save
-    ALERT_1105("alert1105"), // language
-    ALERT_1106("alert1106"), // pri init api
-    ALERT_1107("alert1107"), // sendPostForSms
-    ALERT_1108("alert1108"), // notification impl
-    ALERT_1110("alert1110");  // simp
+    ALERT_1103("alert1303"), // check imei fail
+    ALERT_1104("alert1304"), // saveCheckImeiRequest ,appDeviceDetailsRepo save
+    ALERT_1105("alert1305"), // language
+    ALERT_1106("alert1306"), // pri init api
+    ALERT_1107("alert1307"), // sendPostForSms
+    ALERT_1108("alert1308"), // notification impl
+    ALERT_1110("alert1309");  // simp
 
 
     private String name;
